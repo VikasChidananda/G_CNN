169567f6ae5650aae59ba048a58b92682f6a500c
cvikas <vikasc.nitk@gmail.com>
2023-02-20 04:16:45 +0100

    add indices from matrix

--- src/transformations.jl
+++ src/transformations.jl
@@ -7,3 +7,5 @@ using Plots
 
 # getting indices off of a matrix
 gaussian_kernel     =   Kernel.gaussian(1)
+Z                   =   OffsetArray(gaussian_kernel, -2:2, -2:2)
+to_indices          =   [c.I for c ∈  CartesianIndices(Z)]
